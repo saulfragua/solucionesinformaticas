18f0fe3552e898947fae143d512dc518ecbcfcb9
saul fragua nova <saul.fragua1988@gmail.com>
2025-01-13 15:43:37 -0500

    Add files via upload

--- js/JavaScript.js
+++ js/JavaScript.js
@@ -25,3 +25,31 @@ document.getElementById('contact-form').addEventListener('submit', function(even
             alert('Error al enviar el correo: ' + error.text);
         });
 });
+
+// Espera a que todo el contenido de la página esté completamente cargado
+window.addEventListener('load', () => {
+    // Selecciona todos los enlaces del menú
+    const menuLinks = document.querySelectorAll('.navbar-nav .nav-item .nav-link');
+  
+    // Añade la clase 'animate' a cada enlace del menú con un pequeño retraso para el efecto secuencial
+    menuLinks.forEach((link, index) => {
+      setTimeout(() => {
+        link.classList.add('animate');
+      }, index * 150); // Ajusta el valor de retraso según lo desees
+    });
+  });
+  
+
+
+
+  // Espera a que todo el contenido de la página esté completamente cargado
+window.addEventListener('load', () => {
+    // Selecciona el logo
+    const logo = document.querySelector('.navbar-brand');
+    
+    // Añade la clase 'animateLogo' al logo para activar la animación
+    logo.classList.add('animateLogo');
+  });
+
+  
+
